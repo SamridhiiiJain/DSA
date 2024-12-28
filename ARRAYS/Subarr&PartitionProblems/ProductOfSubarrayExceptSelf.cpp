ccf07bae7ebad63f0a543e4f40d4d6f55f7ab7d8
SamridhiiiJain <jsamridhi66@gmail.com>
2024-12-28 21:39:31 +0530

    productExceptSelf

--- ARRAYS/Subarr&PartitionProblems/ProductOfSubarrayExceptSelf.cpp
+++ ARRAYS/Subarr&PartitionProblems/ProductOfSubarrayExceptSelf.cpp
@@ -25,3 +25,21 @@ vector<int> productExceptSelf(vector<int>& nums) {
     productExceptSelf(arr);
     return 0;
 }
+
+//optimized Approach
+vector<int> productExceptSelf(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>ans(n,1);
+        int i;
+        for(i=1;i<n;i++)
+        {
+            ans[i]=ans[i-1]*nums[i-1];
+        }
+        int suffix=1;
+         for(i=n-2;i>=0;i--)
+        {
+            suffix*=nums[i+1];
+            ans[i]*=suffix;
+        }
+        return ans;
+    }
