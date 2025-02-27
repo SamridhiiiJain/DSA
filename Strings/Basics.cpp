bd42d7bd7f347d9d0866d99f11fac6821c1868b4
SamridhiiiJain <jsamridhi66@gmail.com>
2025-02-27 15:18:29 +0530

    Starting with strings

---
+++ Strings/Basics.cpp
@@ -0,0 +1,37 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+    char str[]={'a','b','c','\0'};
+    cout<< str<<endl;
+    char str1[]={'a','b','c'};
+    cout<< str1<<endl;
+    char str3[]="Hello world"; //String literals->Constants does not change values
+    cout<< strlen(str3)<<endl; // constant pointers
+    cout<< str3[4]<<endl;
+    int str2[]={'1','2','3'};
+    cout<< str2<<endl;
+    
+    //input from user
+    // char sam[100];
+    // cout<<"Enter Character Array"<<endl;
+    // cin>>sam;
+    // cout<<"output: " << sam << endl;
+    
+    // char sam[10];
+    // cout<<"Enter Character Array"<<endl;
+    // cin.getline(sam,10,'$');
+    // cout<<"output: " << sam << endl;
+    
+    char sam[15];
+    cout<<"Enter Character Array"<<endl;
+    cin.getline(sam,15);
+     for(char ch : sam)
+     {
+         cout<< ch <<" ";
+     }
+    cout<< endl;
+    return 0;
+}
