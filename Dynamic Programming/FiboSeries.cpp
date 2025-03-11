3445f13e144cfea8750b49b299283e00bf8fee6e
SamridhiiiJain <jsamridhi66@gmail.com>
2025-03-11 20:35:13 +0530

    1st DP

---
+++ Dynamic Programming/FiboSeries.cpp
@@ -0,0 +1,14 @@
+int fib(int n) {
+    if(n<=1)
+   {
+       return n;
+   }
+   int curr,prev=1,prev2=0;
+   for(int i=2;i<=n;i++)
+   {
+       curr=prev+prev2;
+       prev2=prev;
+       prev=curr;
+   }
+   return curr;
+}
